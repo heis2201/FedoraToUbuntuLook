e110c6ab305ee39b35053a27c89f550d70b8fc81
heis2201 <heis2201@web.de>
2024-06-14 16:03:08 +0200

    randomize name of temporary directory and minor improvements

--- ubuntu_look.sh
+++ ubuntu_look.sh
@@ -8,20 +8,18 @@
 # - yaru theme
 # - gnome-shell-user-extension-user-theme
 # - gnome-tweaks to set icons, theme, mouse pointer later. 
-#
-#
-# install yaru-theme gnome-shell-extension-user-theme gnome-tweaks
-# fyi, Gnome recommends to install gnome-tweaks as flatpak. Change code (commented lines below if desired)
+# - unzip
+
+# install yaru-theme gnome-shell-extension-user-theme gnome-tweaks unzip
 echo "sudo command will only be used once to install:" 
-echo "yaru-theme gnome-shell-extension-user-theme gnome-tweaks"
+echo "yaru-theme gnome-shell-extension-user-theme gnome-tweaks, and unzip"
 echo "All other commands run as regular user commands without elevated priviliges"
 sudo dnf --refresh -y install yaru-theme gnome-shell-extension-user-theme gnome-tweaks
-# sudo dnf --refresh -y install yaru-theme gnome-shell-extension-user-theme
-flat
+# download ubuntu font family to ~/.fonts and unzip
 mkdir -p ~/.fonts
-# donwload ubuntu font family to .fonts and unzip
 curl -o ~/.fonts/ubuntu.zip https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-0.83.zip
 unzip -j ~/.fonts/ubuntu.zip -d ~/.fonts
+# delete txt files and font zip archive
 rm ~/.fonts/*.txt
 rm ~/.fonts/ubuntu.zip
 # update font cache
@@ -42,19 +40,23 @@ gsettings set org.gnome.desktop.interface icon-theme 'Yaru-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
 gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
 
-
 # Install Gogh terminal profile "Clone of Ubuntu"
 # see https://github.com/Gogh-Co/Gogh and https://github.com/Gogh-Co/Gogh/blob/master/installs/clone-of-ubuntu.sh
-TEMP_DL_DIR="$HOME/goghgoghgogh"
-mkdir -p "$TEMP_DL_DIR"
-wget -P "$TEMP_DL_DIR" https://raw.githubusercontent.com/Gogh-Co/Gogh/master/installs/clone-of-ubuntu.sh
-wget -P "$TEMP_DL_DIR" https://raw.githubusercontent.com/Gogh-Co/Gogh/master/apply-colors.sh
-chmod +x "$TEMP_DL_DIR/clone-of-ubuntu.sh"
-"$TEMP_DL_DIR/clone-of-ubuntu.sh"
-rm -rf "$TEMP_DL_DIR"
+
+# generate random directory for gogh script. will be deleted afterwards
+TEMP_DL_DIR=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12)
+mkdir -p "$HOME/$TEMP_DL_DIR"
+# Download and execute Gogh scripts for "Clone of Ubuntu" terminal look
+wget -P "$HOME/$TEMP_DL_DIR" https://raw.githubusercontent.com/Gogh-Co/Gogh/master/installs/clone-of-ubuntu.sh
+wget -P "$HOME/$TEMP_DL_DIR" https://raw.githubusercontent.com/Gogh-Co/Gogh/master/apply-colors.sh
+chmod +x "$HOME/$TEMP_DL_DIR/clone-of-ubuntu.sh"
+chmod +x "$HOME/$TEMP_DL_DIR/apply-colors.sh"
+"$HOME/$TEMP_DL_DIR/clone-of-ubuntu.sh"
+rm -rf "$HOME/$TEMP_DL_DIR"
 
 # didnt manage yet to set "Clone Of Ubuntu" as new default profile.
 # extracting the correct UUID from the gnome terminal profiles didn't work
 # need to set up manually in Terminal
 
 echo "Setup complete!"
+echo "Note: You might need to manually set 'Clone Of Ubuntu' as the default profile in the terminal settings."
